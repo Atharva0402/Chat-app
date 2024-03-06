70c71ada5e9a6200b0245a9e8a99c363d21cb678
Atharva Kadam <100138079+Atharva0402@users.noreply.github.com>
2024-03-06 22:22:46 +0530

    Update APIRoutes.js
    added the host api

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -3,7 +3,8 @@
 // export const loginRoute = `${host}/api/auth/login`;
 // export const setAvatarRoute = `${host}/api/auth/setAvatar`;
 // export const allUsersRoute = `${host}/api/auth/allusers`;
-export const host = "http://localhost:5000";
+// export const host = "http://localhost:5000";
+export const host= "https://chat-erq4eb8xe-atharva0402.vercel.app"
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
